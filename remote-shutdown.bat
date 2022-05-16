5a41836c2ec77602fc1ef9e2c8c85e41dfb5d005
xxmike3 <michaelmartins.js@hotmail.com>
2022-05-16 09:09:39 -0300

    v2.0 - Todos os Logs no mesmo arquivo

--- remote-shutdown.bat
+++ remote-shutdown.bat
@@ -6,24 +6,26 @@
 :: -
 ::  13/05/2022 - Inicio / Criação
 
+echo ----- Iniciado >>log.txt
+echo .              >>log.txt
 setlocal enabledelayedexpansion
 for /F %%i in (IP_LIST.txt) do (
 	:: Verify if the Station it is online
 	ping -n 1 %%i | find "TTL=128" >nul
 	IF !ERRORLEVEL! == 1 (
 	ECHO Address %%i - Offline!
-	echo %%i >> offline.txt
+	echo Micro %%i Offline>> log.txt
 	) else (
 		ECHO Address %%i - Online!
-		echo %%i >> online.txt
+		echo Micro %%i Online>> log.txt
 
 		:: Verify if the computer allow remote access
 		echo Address %%i - Trying access ...
 		if not exist "\\%%i\c\windows\System32" ( 
 			ECHO Address %%i - Online, but, no access.
-			echo %%i >> bloqueada.txt
+			echo Micro %%i sem acesso>> log.txt
 			) else ( 
 
 			:: SHUTDOWN !
-           		 psexec.exe \\%%i -s -u gti -p 6feira*13 shutdown -s -t 00
-			echo %%i >> desligado.txt)))
+           		 psexec.exe \\%%i -s -u gti -p 6feira*13 shutdown -s -t 00 /f
+			echo Micro %%i Desligado>> log.txt)))
